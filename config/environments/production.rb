d30f82f7add5e527fb1e0a7275331e8d73c7bbb8
nguyenquangminh0711 <nguyenquangminh0711@gmail.com>
2016-03-08 23:54:46 +0700

    Fix API endpoint

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
 
   config.paperclip_defaults = {
     storage: :s3,
-    s3_host_name: 's3-website.ap-southeast-1.amazonaws.com',
+    s3_host_name: 's3-ap-southeast-1.amazonaws.com',
     s3_protocol: :https,
     s3_credentials: {
       bucket: "solomoproduction",
