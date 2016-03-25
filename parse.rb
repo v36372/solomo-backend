02d5dac16bb05a7a757c8f1bae619fb0ba8a903b
nguyenquangminh0711 <nguyenquangminh0711@gmail.com>
2016-03-25 10:33:22 +0700

    Enhance map

--- parse.rb
+++ parse.rb
@@ -1,10 +1,18 @@
 require 'httparty'
 require 'byebug'
 CSV.foreach("listdeal.csv", headers: true) do |row|
-  HTTParty.post('https://solomo-api.herokuapp.com/api/v1/posts.json?user_token=jT9jkh4rMrUReQCsweHY',body: {
+  puts "==============="
+  puts "Ready to send"
+  puts "==============="
+  body =  {
       description: row['description'],
-      picture_url: "http:#{row['img']}",
+      picture_url: row['img'],
       post_type: 'crawl',
-      crawl_user_name: row['name']
-  })
+      crawl_user_name: row['name'],
+      location_lat: row['lat'],
+      location_long: row['long']
+  }
+  response = HTTParty.post('https://solomo-api.herokuapp.com/api/v1/posts.json?user_token=jT9jkh4rMrUReQCsweHY',body: body)
+  puts body.inspect
+  puts response
 end
