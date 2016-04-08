41b4a35e95e3a52e099fbdb635a5d7bc9223bf34
nguyenquangminh0711 <nguyenquangminh0711@gmail.com>
2016-04-08 08:40:08 +0700

    FIx mailgun bug

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 class ApplicationMailer < ActionMailer::Base
   add_template_helper(ApplicationHelper)
 
-  default from: "Solomo Team <noreply@#{solomo-api.herokuapp.com}>"
+  default from: "Solomo Team <noreply@#{Rails.application.secrets.mailgun_domain}>"
   layout 'mailer'
 end
